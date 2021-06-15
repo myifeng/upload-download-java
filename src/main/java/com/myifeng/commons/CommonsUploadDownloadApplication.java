9fcd4fcea94631cb090e10ac40d0da731c85b5a1
myifengs <myifengs@gmail.com>
2021-06-15 10:43:34 +0800

    Initialize a web project using Spring boot.

---
+++ src/main/java/com/myifeng/commons/CommonsUploadDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.myifeng.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsUploadDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonsUploadDownloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myifeng/commons/CommonsUploadDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myifeng.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsUploadDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
