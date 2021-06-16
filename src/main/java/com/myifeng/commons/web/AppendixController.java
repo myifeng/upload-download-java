9275b96dd7a469f55e12f922f183fcf7836a8622
myifengs <myifengs@gmail.com>
2021-06-16 16:05:24 +0800

    create AppendixController.

---
+++ src/main/java/com/myifeng/commons/web/AppendixController.java
@@ -0,0 +1,111 @@
+package com.myifeng.commons.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping(value = "/appendix")
+@RestController
+public class AppendixController {
+
+    @Value("${appendix.upload-folder:./}")
+    private String uploadFolder;
+
+    @PostConstruct
+    public void init() throws IOException {
+        var path = Paths.get(uploadFolder);
+        if (!path.toFile().exists()) {
+            Files.createDirectories(path);
+        }
+    }
+
+    /**
+     * Upload single file or multiple files.
+     * Use RequestURI and UUID as subPath.
+     * Return a collection of paths.
+     *
+     * @param request
+     * @return return ["$RequestURI/$UUID/$filename"]
+     * E.g : ["appendix/images/cf109c9e-7662-4c71-9a3d-27d3fd664206/hello.jpg", ...]
+     */
+    @PostMapping(value = "/**")
+    public List<String> fileUpload(HttpServletRequest request) {
+        var lists = ((MultipartHttpServletRequest) request).getMultiFileMap().values();
+
+        var subPath = request.getRequestURI();
+        var relativePath = Paths.get(uploadFolder, subPath);
+        if (!relativePath.toFile().exists()) {
+            try {
+                Files.createDirectories(relativePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("createDirectories " + relativePath + " fail");
+            }
+        }
+        var filePaths = new ArrayList<String>();
+        lists.stream().forEach(files -> files.stream().forEach(file -> {
+            try {
+                var outputFilePath = Paths.get(subPath, UUID.randomUUID().toString(), file.getOriginalFilename());
+                var absolutePath = Paths.get(uploadFolder, outputFilePath.toString());
+                absolutePath.toFile().getParentFile().mkdirs();
+
+                var inputStream = file.getInputStream();
+                var outputStream = Files.newOutputStream(absolutePath);
+                byte[] tempts = new byte[1024];
+                int battered;
+                while ((battered = inputStream.read(tempts)) != -1) {
+                    outputStream.write(tempts, 0, battered);
+                }
+                inputStream.close();
+                outputStream.close();
+                filePaths.add(outputFilePath.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }));
+        return filePaths;
+    }
+
+    /**
+     * Download file
+     *
+     * @param request https://localhost:8080/appendix/images/cf109c9e-7662-4c71-9a3d-27d3fd664206/hello.jpg
+     * @return
+     * @throws IOException
+     */
+    @GetMapping("/**")
+    public ResponseEntity<InputStreamResource> getFile(HttpServletRequest request) throws IOException {
+        var path = Paths.get(uploadFolder, request.getRequestURI());
+        var file = path.toFile();
+        if (file.exists() && file.isFile()) {
+            return ResponseEntity
+                    .ok()
+                    .header(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate")
+                    .header(HttpHeaders.CACHE_CONTROL, "no-cache")
+                    .header(HttpHeaders.PRAGMA, "no-cache")
+                    .header(HttpHeaders.EXPIRES, "0")
+                    .contentType(MediaType.MULTIPART_FORM_DATA)
+                    .contentLength(file.length())
+                    .body(new InputStreamResource(Files.newInputStream(path)));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
